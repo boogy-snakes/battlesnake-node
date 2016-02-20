224b876aca7615697b8d2205a1664928899ef286
tayden-hakai <tayden.denouden@hakai.org>
2016-02-20 13:37:56 -0800

    Resolve merge conflict

--- src/basicAI.js
+++ src/basicAI.js
@@ -1,3 +1,29 @@
 // basic ai, just finds the closest food
 
-module.exports = {};
+module.exports = {
+  findClosestFood: function(data) {
+    // data
+    // {
+    //     "game": "hairy-cheese",
+    //     "mode": "advanced",
+    //     "turn": 0,
+    //     "height": 20,
+    //     "width": 30,
+    //     "snakes": [
+    //         <Snake Object>, <Snake Object>, ...
+    //     ],
+    //     "food": [],
+    //     "walls": [],  // Advanced Only
+    //     "gold": []    // Advanced Only
+          // "pmap": [[][][][]]...
+    // }
+
+    // For each food
+      // For each snake
+        // get the distance to the food
+        // Run pathfinding using probability map with cutoff
+        // Store
+
+    // Return food we have best chance of getting to first
+  }
+};
