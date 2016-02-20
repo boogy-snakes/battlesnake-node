9807702003e0ead1c595b9c70ec4b1c4ee20e2b9
James Hedin <jhedin10@gmail.com>
2016-02-20 12:27:47 -0800

    adds most of the pathfinding

--- src/postprocessor.js
+++ src/postprocessor.js
@@ -8,6 +8,11 @@ pf = Promise.promisifyAll(pf);
 
 module.exports  = function(){
 
+	var finder = new pf.AStarFinder({
+		allowDiagonal: false,
+		dontCrossCorners: true,
+		heuristic: pf.Heuristic.chebyshev
+	});
 	var size = {x:0, y:0};
 
 	return {
@@ -15,9 +20,26 @@ module.exports  = function(){
 			size.x = x;
 			size.y = y;
 		},
-		direct: function(res) {
+		direct: function(map, current, target, cutoff) {
 			
-		}
+			var grid = new pf.Grid(x, y); 
+
+			for(var i = 0; i < size.x; i++) {
+				for(var j = 0; j < size.y; j++) {
+					if(map[i][j] > cutoff) {
+						grid.setWalkableAt(i, j, false);
+					}
+				}
+			}
+
+		var path = finder.find(current.x, current.y, target.x, target.y, grid);
+
+		var next = path[1];
+
+		var h = next[0] - current.x;
+		var v = next[1] - current.y;
+
+		
 	};
 
 };
