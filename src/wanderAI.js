bede07d9649425c821f14322678c35b45c8eb89d
tayden-hakai <tayden.denouden@hakai.org>
2016-02-20 15:46:38 -0800

    Add wanderAi untested

---
+++ src/wanderAI.js
@@ -0,0 +1,66 @@
+module.exports = function(data) {
+  var current = data.current;
+  var pmap = data.pmap;
+
+  var distances = { n: 0, e: 0, s: 0, w: 0 };
+
+  // Calculate north distance
+  var x = current.x;
+  var y = current.y;
+  var z = 0;
+  while(z < 0.5) {
+    y--;
+    z = pmap[y][x];
+    distances.n++;
+  }
+
+  // Calculate east distance
+  x = current.x;
+  y = current.y;
+  z = 0;
+  while(z < 0.5) {
+    x++;
+    z = pmap[y][x];
+    distances.e++;
+  }
+
+  // Calculate south distance
+  x = current.x;
+  y = current.y;
+  z = 0;
+  while(z < 0.5) {
+    y++;
+    z = pmap[y][x];
+    distances.s++;
+  }
+
+  // Calculate west distance
+  x = current.x;
+  y = current.y;
+  z = 0;
+  while(z < 0.5) {
+    x--;
+    z = pmap[y][x];
+    distances.w++;
+  }
+
+  // Get maximum distance in distances
+  var direction = Object.keys(obj).reduce((a, b) => {obj[a] > obj[b] ? a : b });
+
+  switch(direction){
+  case "n":
+    data.target = {x: current.x, y: current.y-1};
+    break;
+  case "e":
+    data.target = {x: current.x+1, y: current.y};
+    break;
+  case "s":
+    data.target = {x: current.x, y: current.y+1};
+    break;
+  case "w":
+    data.target = {x: current.x-1, y: current.y};
+    break;
+  }
+
+  return data;
+};
